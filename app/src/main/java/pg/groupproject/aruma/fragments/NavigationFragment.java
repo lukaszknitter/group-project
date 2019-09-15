a762edfed65635c9d30f68dad7b48902538ca0cc
Mateusz Szypulski <26szypulski2gmail.com>
2019-09-15 21:36:56 +0200

    use osmdroid map instead of google maps

--- app/src/main/java/pg/groupproject/aruma/fragments/NavigationFragment.java
+++ app/src/main/java/pg/groupproject/aruma/fragments/NavigationFragment.java
@@ -1,134 +1,66 @@
 package pg.groupproject.aruma.fragments;
 
 import android.content.Context;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-
-import java.util.List;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.library.BuildConfig;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 import pg.groupproject.aruma.R;
-import pg.groupproject.aruma.feature.MyLocationHandler;
-import pg.groupproject.aruma.feature.location.LocationService;
-import pg.groupproject.aruma.feature.route.Route;
-import pg.groupproject.aruma.feature.route.RouteService;
 
-public class NavigationFragment extends Fragment implements OnMapReadyCallback {
-	private GoogleMap mMap;
-	private LocationManager locationManager;
-	private MyLocationHandler locationHandler;
-	private RouteService routeService;
-	private LocationService locationService;
+public class NavigationFragment extends Fragment {
 
-	private long currentRouteId = -1;
-	private boolean isTrainingRunning = false;
-	private ImageButton trainingButton;
+	private MapView map = null;
 
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		final View inflateView = inflater.inflate(R.layout.fragment_navigation, null);
-		SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-		if (mapFragment != null) {
-			mapFragment.getMapAsync(this);
-		}
+		super.onCreate(savedInstanceState);
+		Context ctx = getContext();
+		Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+		Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
 
-		initializeButtons(inflateView);
-		setRetainInstance(true);
-		routeService = new RouteService(getActivity().getApplicationContext());
-		locationService = new LocationService(getActivity().getApplicationContext());
+		final View inflateView = inflater.inflate(R.layout.fragment_navigation, null);
+		GeoPoint pointGdansk = new GeoPoint(40.741895, -73.989308);
+
+		//inflate and create the map
+		map = (MapView) ((CoordinatorLayout) inflateView).getChildAt(0);
+		map.setTileSource(TileSourceFactory.MAPNIK);
+
+		map.setTilesScaledToDpi(true);
+		map.getController().setZoom(15);
+		map.getController().setCenter(pointGdansk);
+
+		map.setMultiTouchControls(true);
 		return inflateView;
 	}
 
-	/**
-	 * Manipulates the map once available.
-	 * This callback is triggered when the map is ready to be used.
-	 * This is where we can add markers or lines, add listeners or move the camera. In this case,
-	 * we just add a marker near Sydney, Australia.
-	 * If Google Play services is not installed on the device, the user will be prompted to install
-	 * it inside the SupportMapFragment. This method will only be triggered once the user has
-	 * installed Google Play services and returned to the app.
-	 */
-	@Override
-	public void onMapReady(GoogleMap googleMap) {
-		mMap = googleMap;
-		mMap.setMyLocationEnabled(true);
-
-		initializeLocationListener();
-		setMapCamera();
+	public void onResume() {
+		super.onResume();
+		//this will refresh the osmdroid configuration on resuming.
+		Context ctx = getContext();
+		Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+		map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
 	}
 
-	private void initializeButtons(final View view) {
-		trainingButton = view.findViewById(R.id.trainingButton);
-		trainingButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startTraining(view);
-			}
-		});
-	}
-
-	private void startTraining(View view) {
-		if (!this.isTrainingRunning) {
-			currentRouteId = routeService.create();
-			Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-			if (location != null) {
-				locationService.insert(location, currentRouteId);
-			}
-			trainingButton.setImageResource(R.drawable.ic_stop);
-			locationHandler.startTraining();
-			this.isTrainingRunning = true;
-		} else {
-			List<Route> routes = routeService.getAll();
-			Log.i(NavigationFragment.class.getName(),
-					"Got One: " + routeService.get(currentRouteId));
-			Log.i(NavigationFragment.class.getName(),
-					"Got routes: ");
-			routes.forEach(route -> Log.i(NavigationFragment.class.getName(), route.toString()));
-
-			List<pg.groupproject.aruma.feature.location.Location> locations = locationService.getAllByRouteId(currentRouteId);
-			Log.i(NavigationFragment.class.getName(),
-					"Got locations: ");
-			locations.forEach(location -> Log.i(NavigationFragment.class.getName(), location.toString()));
-			trainingButton.setImageResource(R.drawable.ic_start);
-			locationHandler.pauseTraining();
-			this.isTrainingRunning = false;
-		}
-	}
-
-	private void setMapCamera() {
-		Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-		LatLng myLocation;
-		if (location != null) {
-			myLocation = new LatLng(location.getLatitude(), location.getLongitude());
-		} else {
-			myLocation = new LatLng(54.005025, 15.989035);
-		}
-		mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
-		mMap.animateCamera(CameraUpdateFactory.zoomTo(18));
-	}
-
-	private void initializeLocationListener() {
-		locationHandler = new MyLocationHandler(this, mMap);
-		locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-		/** W linijce niżej pierwszy parametr mówi, czy korzystamy z GPS, sieci, wifi etc. To bedzie trzeba sparametryzować,
-		 żeby wybierało najlepszą możliwą udostępnioną przez użytkownika opcję*/
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationHandler);
-		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 10, locationHandler);
+	public void onPause() {
+		super.onPause();
+		//this will refresh the osmdroid configuration on resuming.
+		Context ctx = getContext();
+		Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+		map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
 	}
 }
