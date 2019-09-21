c1839cf33325b3cb21eee7dde6309ca283fe4bae
Mateusz Szypulski <26szypulski2gmail.com>
2019-09-21 22:02:15 +0200

    add routing to

--- app/src/main/java/pg/groupproject/aruma/fragments/NavigationFragment.java
+++ app/src/main/java/pg/groupproject/aruma/fragments/NavigationFragment.java
@@ -1,29 +1,50 @@
 package pg.groupproject.aruma.fragments;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
 
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
 import org.osmdroid.library.BuildConfig;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Polyline;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+import java.util.ArrayList;
+
+import pg.groupproject.aruma.MyLocationListener;
 import pg.groupproject.aruma.R;
 
 public class NavigationFragment extends Fragment {
 
 	private MapView map = null;
+	private LocationManager locationManager;
+	private MyLocationListener myLocationListener;
+	private Location currentLocation;
+	private RoadManager roadManager;
 
 	@Nullable
 	@Override
@@ -33,19 +54,41 @@ public class NavigationFragment extends Fragment {
 		Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
 		Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
 
-		final View inflateView = inflater.inflate(R.layout.fragment_navigation, null);
-		GeoPoint pointGdansk = new GeoPoint(40.741895, -73.989308);
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
 
+		final View inflateView = inflater.inflate(R.layout.fragment_navigation, null);
+		roadManager = new OSRMRoadManager(getActivity());
+		initializeLocationManager();
+		initializeMap(inflateView);
+		initializeRoutingFeatures();
+
+		return inflateView;
+	}
+
+	private void initializeLocationManager(){
+		myLocationListener = new MyLocationListener();
+		locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+		//TODO dodać pytani o uprawnienia
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,myLocationListener);
+	}
+
+	private void initializeMap(View inflateView){
 		//inflate and create the map
 		map = (MapView) ((CoordinatorLayout) inflateView).getChildAt(0);
-		map.setTileSource(TileSourceFactory.MAPNIK);
+		map.setTileSource(TileSourceFactory.OpenTopo);
 
 		map.setTilesScaledToDpi(true);
 		map.getController().setZoom(15);
-		map.getController().setCenter(pointGdansk);
-
+		//TODO Jak zając się tym, gdy nic sie nie zwroci?
+		currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		map.getController().setCenter(new GeoPoint(currentLocation.getLatitude(),currentLocation.getLongitude()));
 		map.setMultiTouchControls(true);
-		return inflateView;
+
+		MyLocationNewOverlay myLocationNewOverlay = new MyLocationNewOverlay(map);
+		myLocationNewOverlay.enableFollowLocation();
+		myLocationNewOverlay.enableMyLocation();
+		map.getOverlays().add(myLocationNewOverlay);
 	}
 
 	public void onResume() {
@@ -63,4 +106,53 @@ public class NavigationFragment extends Fragment {
 		Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
 		map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
 	}
+
+	private void initializeRoutingFeatures() {
+		MapEventsReceiver mReceiver = new MapEventsReceiver() {
+			@Override
+			public boolean singleTapConfirmedHelper(GeoPoint p) {
+				return false;
+			}
+
+			@Override
+			public boolean longPressHelper(GeoPoint p) {
+				Context ctx = getContext();
+
+				Dialog dialog = new Dialog(ctx);
+				dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+				dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+				dialog.setContentView(R.layout.navigate_dialog);
+
+				TextView routePlaceName = (TextView) dialog.findViewById(R.id.route_place_name) ;
+				routePlaceName.setText("Latitiude: "+ p.getLatitude() + "\nLongitude: "+p.getLongitude());
+
+				initializeGoToButton((ImageView)dialog.findViewById(R.id.go_to_arrow), ctx, new GeoPoint(p.getLatitude(), p.getLongitude()), dialog);
+
+				dialog.show();
+
+				return false;
+			}
+		};
+
+		map.getOverlays().add(new MapEventsOverlay(mReceiver));
+	}
+
+	private void initializeGoToButton(ImageView imageView, Context ctx, GeoPoint destinationPoint, Dialog dialog) {
+		imageView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				ArrayList<GeoPoint> points = new ArrayList<GeoPoint>();
+				points.add(new GeoPoint(currentLocation.getLatitude(), currentLocation.getLongitude()));
+				points.add(destinationPoint);
+				Road road = roadManager.getRoad(points);
+				Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+				roadOverlay.setWidth(10);
+				map.getOverlays().add(roadOverlay);
+				map.invalidate();
+				dialog.hide();
+			}
+		});
+	}
 }
+
+
